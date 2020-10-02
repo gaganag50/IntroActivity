97c8896ec63931b8bc2ca93c50a5757fff9ddeba
Gagan Aggarwal <gaganag50@gmail.com>
2020-10-02 20:25:24 +0530

    sharedPreference added

---
+++ app/src/main/java/com/examples/introactivity/BaseActivity.kt
@@ -0,0 +1,30 @@
+package com.examples.introactivity
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.examples.introactivity.Fragments.LaunchActivity
+
+class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val sharedPref = getSharedPreferences(
+            getString(R.string.preference_file_key), Context.MODE_PRIVATE
+        )
+        if (sharedPref == null) {
+            startActivity(Intent(this, LaunchActivity::class.java))
+        } else {
+            val defaultValue = resources.getInteger(R.integer.introduction)
+            val showIntroduction = sharedPref.getInt(getString(R.string.intro), defaultValue)
+
+            if (showIntroduction == 1) {
+
+                startActivity(Intent(this, LaunchActivity::class.java))
+            } else {
+                startActivity(Intent(this, TransformationActivity::class.java))
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/examples/introactivity/Fragments/LaunchActivity.kt
+++ app/src/main/java/com/examples/introactivity/Fragments/LaunchActivity.kt
@@ -1,13 +1,30 @@
 package com.examples.introactivity.Fragments
 
+import android.content.Context
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.examples.introactivity.R
 
-class LaunchActivity :AppCompatActivity(){
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+
+class LaunchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.temp)
+        val sharedPref = getSharedPreferences(
+            getString(R.string.preference_file_key), Context.MODE_PRIVATE
+        )
+        sharedPref?.let {
+            with(it.edit()) {
+
+
+                putInt(getString(R.string.intro), 1)
+                apply()
+            }
+        }
+
     }
+
+
+
+
 }
